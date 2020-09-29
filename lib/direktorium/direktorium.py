5d3a2940f7066066566e23c552c1ed5186fda22c
Matthias Gianfelice <matthias@gianfelice.de>
2020-09-29 11:20:23 +0200

    Direktoriu fertig programmiert.

--- lib/direktorium/direktorium.py
+++ lib/direktorium/direktorium.py
@@ -1,20 +1,51 @@
+from dataclasses import dataclass
 from datetime import date
+import json
+import os
 import requests
 from typing import List
 
 from .event import Event
 
 
+@dataclass
 class Direktorium:
 
-    def __init__(self, kalender='deutschland'):
-        self.kalender = kalender
+    kalender: str = 'deutschland'
+    cache_dir: str = None
 
     def get(self, d: date) -> List[Event]:
-        r = requests.get('http://www.eucharistiefeier.de/lk/api.php?'
-                         'format=json&info=wdtrgflu&dup=e&bahn=j&'
-                         f'kal={self.kalender}&'
-                         f'jahr={d.year}&monat={d.month}&tag={d.day}')
+        r = self.request_cache(d)
         entries = [Event.parse(e) for e in r.json()['Zelebrationen'].values()]
         entries.sort(key=lambda e: e.importance)
         return entries
+
+    def request_api(
+        self, year: int, month: int = None, day: int = None
+    ) -> requests.models.Response:
+        url = 'http://www.eucharistiefeier.de/lk/api.php?format=json&' \
+              f'info=wdtrgflu&dup=e&bahn=j&kal={self.kalender}&jahr={year}&'
+        if month: url += f'monat={month}&'
+        if month and day: url += f'tag={day}&'
+        return requests.get(url)
+
+    def request_cache(self, d: date) -> dict:
+        if self.cache_dir is None:
+            return self.request_api(d.year, d.month, d.day)
+
+        # Daten einlesen (ggf. herunterladen)
+        dir = os.path.join(self.cache_dir, self.kalender)
+        file = os.path.join(dir, f'{d.year}.json')
+        if not os.path.exists(file):
+            if not os.path.exists(dir): os.makedirs(dir)
+            r = self.request_api(d.year)
+            with open(file, 'wb') as f: f.write(r.content)
+            data = r.json()
+        else:
+            with open(file) as f: data = json.load(f)
+
+        # Dictionary für aktuellen Tag konstruieren
+        datestr = d.isoformat()
+        data['Zelebrationen'] = {k: v for k, v in data['Zelebrationen'].items()
+                                 if datestr == v['Datum']}
+        return data
